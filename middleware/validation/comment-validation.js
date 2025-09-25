dad65df12ea9b52f3f534a3113cf1e7cb1cb12cf
Grant Collins <grantcollins03@gmail.com>
2025-09-25 08:07:05 -0400

    change matches regex for search

--- middleware/validation/comment-validation.js
+++ middleware/validation/comment-validation.js
@@ -10,8 +10,8 @@ const validateGetComments = [
       "Search term must be less than 50 characters and greater than 1."
     )
     .bail()
-    .matches(/^[a-zA-Z0-9\s@.-]+$/)
-    .withMessage("Invalid search format."),
+     .matches(/^[a-zA-Z0-9\s@.\-_,;:()'"!?&+=/#\u00C0-\u017F]+$/)
+    .withMessage("Search contains invalid characters."),
   query("page")
     .optional()
     .isInt({ min: 1, max: 1000 })
